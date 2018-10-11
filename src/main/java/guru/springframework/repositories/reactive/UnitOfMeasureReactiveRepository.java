33c09871128f6d89abe0162e693feae3b70634ae
Robert Fowler <robertf@ipay.co.za>
2018-10-11 21:29:35 +0200

    332. Assignment Review - Convert to Reactive Repositories

--- src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -2,11 +2,12 @@ package guru.springframework.repositories.reactive;
 
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by robertf on 2018/10/09
  */
 public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
-
+    Mono<UnitOfMeasure> findByDescription(String description);
 
 }
